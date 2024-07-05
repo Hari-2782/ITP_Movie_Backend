463395193ed10431402dc4e992f14cbdfe17431f
hari27 <hmsanth@gmail.com>
2024-07-05 11:44:41 +0530

    mm

--- utils/db.js
+++ utils/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 require('dotenv').config();
 
 
-mongoose.connect(process.env.MONGO_URL).then(
+mongoose.connect(process.env.MONGO_URI).then(
     () => {
         console.log('Connected to database');
     }
